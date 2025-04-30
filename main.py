0018d58a3bca76ab99f9850dd30201c256106f18
Long <lytlong.pers@gmail.com>
2025-04-30 10:39:22 +0700

    web framework, restructuring database

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+def main():
+    switch_page("Learner/Dashboard")
+
+if __name__ == "__main__":
+    main()
+
+    
