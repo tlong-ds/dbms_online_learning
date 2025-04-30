edee0c637c728949893d52953549407761d7c9d0
Tran Tuan <trantuan1701ltv@gmail.com>
2025-04-30 16:41:03 +0700

    Authenticafication

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
-from style.sidebar import SideBar
-
-st.session_state["login"] = "Learner"
-SideBar.show()
+#from streamlit_extras.switch_page_button import switch_page
+#from style.sidebar import SideBar
+from pages.Authentification import show_auth_ui
+# st.session_state["login"] = "Learner"
+# SideBar.show()
 
 def main():
-    pass
+    if "login" not in st.session_state:
+        st.session_state["login"] = None
+    show_auth_ui()
     
 
 if __name__ == "__main__":
