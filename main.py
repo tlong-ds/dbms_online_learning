99c1fec6d0eac2541553d78c595776306022aa2e
Long <lytlong.pers@gmail.com>
2025-04-30 16:26:11 +0700

    better visual

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
-from style.sidebar import SideBar
+from style.ui import Visual
+import os
+import toml
 
-st.session_state["login"] = "Learner"
-SideBar.show()
+st.session_state["login"] = True
+st.session_state["name"] = "Ly Thanh Long"
+st.session_state["role"] = "Learner"
+
+
+Visual.initial()
+
 
 def main():
     pass
--- style/ui.py
+++ style/ui.py
@@ -1,17 +1,25 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
 from style.CSSHandle import load_css
+from streamlit_extras.switch_page_button import switch_page
+import os
+import toml
 
-class SideBar:
+
+
+class Visual:
+    FOLDER = "style"
+    THEME = toml.load(".streamlit/config.toml")["theme"]["base"] 
+    LOGO = THEME + "_logo.webp" if THEME else None
+    CSS = "style.css"
+    
     @classmethod
-    def show(cls):
-        load_css("style/style.css")
+    def custom_sidebar(cls):
         with st.sidebar:
             # Logo for web
-
-
+            if cls.LOGO:
+                st.image(os.path.join(cls.FOLDER, cls.LOGO))
             # Learner Navigation
-            if st.session_state.login == "Learner":
+            if st.session_state.login == True and st.session_state.role == "Learner":
                 if st.button("Dashboard"):
                     switch_page("Learner Dashboard")
                 if st.button("Courses"):
@@ -21,14 +29,24 @@ class SideBar:
                 if st.button("Timer"):
                     switch_page("Learner Timer")
             # Instructor
-            if st.session_state.login == "Instructor":
+            if st.session_state.login == True and st.session_state.role == "Instructor":
                 if st.button("Dashboard"):
                     switch_page("Instructor Dashboard")
                 if st.button("Courses"):
                     switch_page("Instructor Courses") 
-            
+            st.divider()
             if st.button("Settings"):
                 switch_page("Settings")
             if st.button("Feedback"):
                 switch_page("Feedback")
-            
+            st.divider()
+            if st.session_state.login == True:
+                if st.button("Log out"):
+                    pass
+    
+    @classmethod
+    def initial(cls):
+        load_css(os.path.join(cls.FOLDER, cls.CSS))
+        cls.custom_sidebar()
+
+    
